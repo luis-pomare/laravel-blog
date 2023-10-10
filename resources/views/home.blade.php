d9ee90fbf82ef35ecbca391163d7c364a044755a
Luis Pomare <luispomare92@gmail.com>
2023-10-10 13:51:56 -0500

    Change root from welcome to home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+</head>
+<body>
+  <header>
+    <h1>Green book</h1>
+    <p>Where you can connect with fellow earth lovers</p>
+  </header>
+</body>
+</html>
