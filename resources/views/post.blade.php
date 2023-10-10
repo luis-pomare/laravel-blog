eabe1fc2a32fc2fd5481db23e6a73737b14561e0
Luis Pomare <luispomare92@gmail.com>
2023-10-10 15:59:22 -0500

    Create post view

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <article>
+            <h2>My first complete post</h2>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. In,
+                itaque ad. Harum at ratione cum error atque consequatur tempora
+                fugit. Ipsum facere error repellat harum ducimus esse tempore
+                repudiandae veritatis.
+            </p>
+        </article>
+    </body>
+</html>
